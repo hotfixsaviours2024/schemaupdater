362080afbfda9958f9a4a250a5dd500a98a0285d
Aggarwal <yash.aggarwal@capgemini.com>
2024-11-05 15:35:23 +0530

    Initial commit

---
+++ src/main/java/com/hotfix/xdms/schemaupdater/SchemaupdaterApplication.java
@@ -0,0 +1,13 @@
+package com.hotfix.xdms.schemaupdater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchemaupdaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchemaupdaterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotfix/xdms/schemaupdater/SchemaupdaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotfix.xdms.schemaupdater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchemaupdaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
